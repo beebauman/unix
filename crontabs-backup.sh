0a30b0a3419e3419c4e6b730afc23d9a956cb21c
Binyamin Bauman <beebauman@gmail.com>
2014-05-29 13:11:55 -0400

    Changes.

--- crontabs-backup.sh
+++ crontabs-backup.sh
@@ -7,7 +7,8 @@
 for user in $(cut -f1 -d: /etc/passwd)
 do
 	crontab=`crontab -u $user -l 2>&1`
-	if [ "$crontab" != "no crontab for $user" ] && [ "$crontab" != "must be privileged to use -u" ] then
+	if [ "$crontab" != "no crontab for $user" ] && [ "$crontab" != "must be privileged to use -u" ]
+	then
     		echo "writing $user.crontab backup file"
 		echo "$crontab" > /var/backups/crontabs/$user.crontab
 	fi
